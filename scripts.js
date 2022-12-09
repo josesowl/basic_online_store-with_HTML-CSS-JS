2290b6f50b6dd2855788c0c4fd24fa8e0e7ec403
José Montes <jose.montesm@autonoma.edu.co>
2022-12-09 16:38:17 -0500

    shoping cart aside added, still missing functionality in JavaScript

--- scripts.js
+++ scripts.js
@@ -1,8 +1,17 @@
 const menuEmail = document.querySelector('.navbar-email');
 const profileMenu = document.querySelector('.profile-menu');
+const shoppingCartIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.shopping-cart1');
 const arrow = document.querySelector('.arrow');
 const iconMenu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+
+//for click in "shopping cart"
+shoppingCartIcon.addEventListener('click', toggleAside);
+
+function toggleAside() {
+    aside.classList.toggle('inactive');
+}
 
 //Mobile menu hide/show
 iconMenu.addEventListener('click', toggleMobileMenu);
@@ -13,14 +22,10 @@ function toggleMobileMenu() {
 
 //for mouseover in "Email"
 menuEmail.addEventListener('click', toggleProfileMenu);
-//menuEmail.addEventListener('mouseout', outProfileMenu);
 
 function toggleProfileMenu() {
     profileMenu.classList.toggle('inactive');
 }
-//function outProfileMenu() {
-//    profileMenu.classList.toggle('inactive');
-//}
 
 //For the arrow to rotate when "Emails" mouseover
 menuEmail.addEventListener('mouseover', arrowDown);
